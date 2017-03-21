e04686bc67b77e0d13381a37f885fb3a9fc505f0
Katerine Muñoz <katerine@appfactory.cl>
2017-03-20 21:49:36 -0300

    Almost finished [minor detail on keeping track of the current state]

--- dfa.c
+++ dfa.c
@@ -1,11 +1,27 @@
 /*
 *   Author: Katerine Muñoz Tello & Alfonso Cabargas Madrid
-*   Description: emulate the transition function of an deterministic finite automata (DFA) 
+*   Description: emulate the transition function of a deterministic finite automata (DFA) 
 */
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
+void latexprint(int wo[1000], int ri, int piv, int w);
+
+void latexprint(int wo[1000], int ri, int piv, int w){
+  printf("$ ");
+  for(int i=0;i<w;i=i+1){
+    if(piv == i){
+      printf("q_%d", ri);
+    }
+    printf("%d", wo[i]);
+  }
+
+  if(piv == w){
+    printf("q_%d $\n", ri);
+  }
+}
 
 int main(){
   int i, l, aux1, aux2, M[1000][2];         
@@ -17,7 +33,7 @@ int main(){
     M[i][1] = aux2;
   }
 
-  int r, aux3, rn[1000];                        //Ask the quantity of final states
+  int r, aux3, rn[l];                          //Ask the quantity of final states
   scanf("%d", &r);
 
   for(i=0;i<r;i=i+1){                           //Ask the finals states
@@ -25,11 +41,28 @@ int main(){
     rn[i] = aux3;           
   }
 
-  int w;                                        
-  scanf("%d", &w);                              //Ask the string (in this case the chain of 0s and 1s) 
+  int w, aux4, wo[1000];                        //Ask the lenght of the string
+  scanf("%d", &w);
 
-  
+  for(i=0;i<w;i=i+1){                           //Ask the string 
+    scanf("%d ", &aux4);
+    wo[i] = aux4;
+  }
 
-  
+  int ri = 1, piv = 0;
+  latexprint(wo, ri, piv, w);
+
+  for(i=0;i<w;i=i+1){
+    printf(" \\vdash $\n");
+    if(wo[i] == 0){
+      ri = M[i][0];
+    }else{
+      ri = M[i][1];
+    }
+    piv = piv + 1;
+
+    latexprint(wo, ri, piv, w);
+  }
+
   return 0;
 }
